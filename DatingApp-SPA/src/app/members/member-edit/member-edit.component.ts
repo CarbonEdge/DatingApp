5a42454b385b25b8584235e288d58c0483161841
CarbonEdge <wholetree65@gmail.com>
2018-09-10 22:25:52 +0200

    s10l99

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { User } from '../../_models/user';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from '../../_services/alertify.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm')
+  editForm: NgForm;
+  user: User;
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotifcation($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(
+    private router: ActivatedRoute,
+    private alertify: AlertifyService
+  ) {}
+
+  ngOnInit() {
+    this.router.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile update successfully');
+
+    this.editForm.reset(this.user);
+  }
+}
